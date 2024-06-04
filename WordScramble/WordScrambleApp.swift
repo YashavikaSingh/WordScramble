3f468c75882a58db98cd01d53ef1cdb776a391ee
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-06-04 22:43:49 +0000

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yashavika Singh on 04.06.24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
